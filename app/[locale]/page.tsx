68096e59d71154289db830bebb1536fbdf635836
Guillermo.dev <guillermo.lucia.dev@gmail.com>
2025-09-21 22:43:51 +0200

    feat: Add Login screen with React Hook Form and Zustand integration

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,13 @@
 
 
-export default function Home() {
-  return (<div> home </div>)
+import { Login } from '@/screens/Login/Login';
+
+export default async function Home({
+  params
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  
+  return <Login locale={locale} />;
 }
